fa8af362b2ba1a099881ee05606f9e4e66239c1c
Nzakou Cédric <nzakou.cedric@protonmail.com>
2018-07-16 06:09:38 +0200

    cucumber test center

--- features/scenarios/load_map.feature
+++ features/scenarios/load_map.feature
@@ -2,6 +2,7 @@ Feature: I go to the main page I see road disprutions
   As a customer
   I wan to be able to see road disruptions
 
+  @javascript
   Scenario: Load road dissruption
     Given I am on the home page
     Then I see the disruptions on the road
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,18 @@ require 'simplecov'
 require 'cucumber/rails'
 require 'etc'
 require 'pp'
+
+require 'capybara/poltergeist'
+Capybara.register_driver :poltergeist do |app|
+  options = {
+      :js_errors => false,
+      :timeout => 120,
+      :debug => false,
+      :phantomjs_options => ['--load-images=no', '--disk-cache=true'],
+      :inspector => true,
+      :phantomjs => Phantomjs.path
+  }
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
+Capybara.javascript_driver = :poltergeist
